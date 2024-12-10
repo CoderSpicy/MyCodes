c7f3b52aae27bfa6c8dcb491875138793a864189
CoderSpicy <leuteriodm@gmail.com>
2024-12-10 23:53:39 +0800

    Add files via upload

---
+++ GradesCalculator/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.ComponentModel.Design;
+namespace ICT
+{
+    class GradesCalculator
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Enter the number of student: ");
+                string user1 = Console.ReadLine();
+                int index1;
+
+                if (!int.TryParse(user1, out index1))
+                {
+                    Console.WriteLine("Invalid input");
+                }
+
+                string[] students = new string[index1];
+                int[] grades = new int[index1];
+
+                for (int i = 0; i < students.Length; i++)
+                {
+                    Console.Write("\nEnter the name of student " + (i + 1) + ": ");
+                    students[i] = Console.ReadLine();
+
+                    Console.Write("Enter " + students[i] + " grades: ");
+                    string user2 = Console.ReadLine();
+
+
+                    if (!int.TryParse(user2, out grades[i]))
+                    {
+                        Console.WriteLine("\nInvalid input\n");
+                        break;
+                    }                   
+                }
+
+                Console.WriteLine("\nResult:");
+
+                for (int i = 0; i < students.Length; i++)
+                {
+                    
+                    Console.Write(students[i] + " - ");
+
+                    if (grades[i] > 100)
+                    {
+                        Console.WriteLine("Grade must be 100 below");
+                        
+                    }
+                    else if (grades[i] == 100)
+                    {
+                        Console.WriteLine("Perfect");
+                        
+                    }
+                    else if (grades[i] < 75)
+                    {
+                        Console.WriteLine("Fail");
+
+                    }
+                    else if (grades[i] < 100)
+                    {
+                        Console.WriteLine("Pass");
+                        
+                    }                 
+                    else
+                    {
+                        Console.WriteLine("Invalid input");
+
+                    }
+                }
+
+                Console.ReadKey();
+                break;
+
+            }
+        }
+    }
+}
---
+++ RockPaperScissor/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+namespace ICT
+{
+    class Game
+    {
+        static void Main(string[] args)
+        {
+            string[] player = { "0", "Rock", "Paper", "Scissors" };
+
+            Console.WriteLine("Welcome to my game!\n");
+            Console.WriteLine("Select '1' for Rock");
+            Console.WriteLine("Select '2' for Paper");
+            Console.WriteLine("Select '3' for Scissors");
+            Console.Write("Enter a number: ");
+            string decision = Console.ReadLine();
+
+            string playersChoice = PlayersChoice(player, decision);
+            string bot = BotDecision();
+
+            Console.WriteLine("\nYou chose: " + playersChoice);
+            Console.WriteLine("The bot chose: " + bot);
+
+            string result = GameMechanics(playersChoice, bot);
+            Console.WriteLine(result);
+        }
+        static string PlayersChoice(string[] player, string decision)
+        {
+            int decided;
+            if (!int.TryParse(decision, out decided) || decided < 1 || decided > 3)
+            {
+                Console.WriteLine("\nInput must be 1-3");
+                return "invalid";
+            }
+            return player[decided];
+        }
+        static string BotDecision()
+        {
+            string[] bot = { "Rock", "Paper", "Scissors" };
+            Random botDecision = new Random();
+
+            int index = botDecision.Next(bot.Length);
+
+            return bot[index];
+        }
+        static string GameMechanics(string playersChoice, string bot)
+        {
+            if (playersChoice == "invalid")
+            {
+                Console.WriteLine("Retry the program.");
+            }
+            
+            if (playersChoice == bot)
+            {
+                Console.WriteLine("DRAW");
+            }
+
+            if ((playersChoice == "Rock" && bot == "Scissors") ||
+                 (playersChoice == "Paper" && bot == "Rock") ||
+                 (playersChoice == "Scissors" && bot == "Paper" ))
+            {
+                return "\nYou WIN";
+            }
+            else
+            {
+                return "\nYou LOSE";
+            }
+        }
+    }
+}
