46a77522a295698da1ce0b809ab478ace1385ee9
CoderSpicy <leuteriodm@gmail.com>
2024-12-10 23:52:32 +0800

    Add files via upload

---
+++ date/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+namespace ICT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // displaying the days
+            string[] days = { "null", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+
+            Console.WriteLine("date selections :\n");
+            int user;
+            int i = 1;
+
+            while (i < days.Length)
+            {
+                Console.WriteLine(days[i]);
+                i++;
+            }
+            //=========================================================================================================
+
+
+            //using loop in user input to prevent error
+            while (true)
+            {
+                Console.Write("\nEnter a number between 1-7 : ");
+                user = Convert.ToInt32(Console.ReadLine());
+
+                //Using if/else to prevent the user not entering 1-7 numbers
+                if (user < 1 || user > 7)
+                {
+                    Console.WriteLine("Invalid input. Try again");
+                    //if the input is not between 1-7, the loop will start again and restart the question
+                }
+                else
+                {
+                    break;
+                    //if the user input is valid the loop will break and proceed to the next line of code
+                }
+            }
+            
+            //do-while di ko gets yawa basta yan
+            do
+            {
+                Console.WriteLine(days[user]);
+                user++;
+            }
+            while (user > days.Length);
+        }
+    }
+}
